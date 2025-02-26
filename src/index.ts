10bc5dea31263a61e6cde8ac109a7a9cdeef5c47
Gustavo Rocha <gurocha05@gmail.com>
2025-02-26 20:57:12 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -92,6 +92,19 @@ import ts from "typescript";
             form.reset();
             return new Todo(todoDescription);
         },
+        getReminder(form: HTMLFormElement) : Reminder {
+            const reminderNotifications = [
+                form.notifications.value as NotificationPlatform,
+            ];
+            const reminderDate = new Date(form.reminderDate.value);
+            const reminderDescription = form.reminderDescription.value;
+            form.reset();
+            return new Reminder(
+                reminderDescription,
+                reminderDate,
+                reminderNotifications
+            );
+        },
         render(tasks: Array<Task>, mode: ViewMode) {
             const tasksList = document.getElementById('tasksList');
             while(tasksList?.firstChild) {
@@ -122,11 +135,21 @@ import ts from "typescript";
     };
 
     const TaskController = (view: typeof taskView) => {
-        const tasks: Array<Task> = [todo, remider];
+        const tasks: Array<Task> = [];
         let mode: ViewMode = ViewMode.TODO;
 
         const handleEvent = (event: Event) => {
             event.preventDefault();
+            const form = event.target as HTMLFormElement;
+            switch (mode as ViewMode) {
+                case ViewMode.TODO:
+                    tasks.push(view.getTodo(form));
+                    break;
+                case ViewMode.REMINDER:
+                    tasks.push(view.getReminder(form));
+                    break;
+            }
+
             view.render(tasks, mode);
         };
 
